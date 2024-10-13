d47ec209776bd7f21e4b40a1615e543ec37d3504
shpokas <s.shpokas@gmail.com>
2024-10-13 13:14:44 +0300

    Logic for calc total cost based on days, type,quickpickup,etc.

--- bookstopAPI/Controllers/ReservationsController.cs
+++ bookstopAPI/Controllers/ReservationsController.cs
@@ -25,5 +25,28 @@ public class ReservationsController : ControllerBase
       return NotFound("Book not found.");
     }
 
+    //Calculate total cost based on days, type,quick pickup, etc.
+    decimal dailyRate = reservation.IsAudiobook ? 3 : 2;
+    decimal totalCost = dailyRate * reservation.Days;
+    if (reservation.Days > 3)
+    {
+      totalCost -= totalCost * 0.1;
+    }
+    if(reservation.Days > 10)
+    {
+      totalCost -= totalCost * 0.2;
+    }
+    totalCost += 3; //This is service fee
+    if(reservation.IsQuickPickUp)
+    {
+      totalCost += 5; //Quick pickup fee
+    }
+
+    reservation.TotalCost = totalCost;
+    _context.Reservations.Add(reservation);
+    await _context.SaveChangesAsync();
+
+    return CreatedAtAction("GetReservation", new { id = reservation.Id}, reservation);
+ 
   }
 }
