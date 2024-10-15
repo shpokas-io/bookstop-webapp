45c04b968afa6d29a6bf0c8e96b38ec9171ebada
shpokas <s.shpokas@gmail.com>
2024-10-15 09:21:33 +0300

    Bug fixed - typo mistake, no space between bracket and transition

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -45,7 +45,7 @@ export default function Header() {
       <div
         className={`fixed top-0 right-0 h-full w-64 bg-blue-700 transform ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
-        }transition-transform duration-300 ease-in-out lg:hidden z-50`}
+        } transition-transform duration-300 ease-in-out lg:hidden z-50`}
       >
         <button onClick={toggleMenu} className="p-4">
           <svg
