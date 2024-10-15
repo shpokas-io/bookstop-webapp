a399ff41ca990e3970dac18ab9967a63bba5b45e
shpokas <s.shpokas@gmail.com>
2024-10-15 09:33:05 +0300

    Installed react routing -> setting up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import HomePage from "./HomePage";
+import ReservationsPage from "./ReservationsPage";
 
 function App() {
   return (
--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,9 +1,9 @@
-export default function BookCard({ book }) {
+export default function BookCard() {
   return (
     <div className="border rounded-lg p4">
-      <h2 className="font-bold">{book.title}</h2>
-      <p>{book.author}</p>
-      <p>{book.price}</p>
+      {/* <h2 className="font-bold">{book.title}</h2> */}
+      {/* <p>{book.author}</p> */}
+      {/* <p>{book.price}</p> */}
       <button className="bg-blue-500 text-white px-4 py-2 mt-2 rounded">
         Reserve
       </button>
