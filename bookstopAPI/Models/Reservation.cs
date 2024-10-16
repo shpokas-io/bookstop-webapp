ece45977aa8a3d6bc079ad973644ecdd8045bc13
shpokas <s.shpokas@gmail.com>
2024-10-16 11:09:36 +0300

    Update

--- bookstopAPI/Models/Reservation.cs
+++ bookstopAPI/Models/Reservation.cs
@@ -4,8 +4,8 @@ public class Reservation
   public int Id {get;set;}
   public int BookId {get;set;}
   public string UserId {get;set;}
-  public string BookType {get;set;}//Audio or Book
-  public int Duration {get;set;} //number of days
-  public bool QuickPickup {get;set;}
+  public string IsAudiobook {get;set;}//Audio or Book
+  public int Days {get;set;} //number of days
+  public bool IsQuickPickUp {get;set;}
   public decimal TotalCost {get;set;}
 }
