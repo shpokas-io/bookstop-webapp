cb7551ee649c0a621f31ddab63c961295f52d906
shpokas <s.shpokas@gmail.com>
2024-10-17 11:40:59 +0300

    FInall build

--- bookstopAPI/obj/Debug/net8.0/bookstopAPI.AssemblyInfo.cs
+++ bookstopAPI/obj/Debug/net8.0/bookstopAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bookstopAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc3c56bfc29c29f8b7c22438ba0048243966ecc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b78e2391696089799494390774e09566f54f0dad")]
 [assembly: System.Reflection.AssemblyProductAttribute("bookstopAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bookstopAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
